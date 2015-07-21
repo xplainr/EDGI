9bb26f5738e63fc625bb7ead3f2a8f48891dacbc
Alfred Markut Jr <alfredm@Macintosh.local>
2015-07-21 14:07:50 -0700

    initial push

---
+++ hackathon/utils.py
@@ -0,0 +1,47 @@
+from rauth import OAuth2Service
+from rauth.session import OAuth2Session
+
+from django.conf import settings
+import json
+
+def get_oauth_service():
+	return OAuth2Service (
+		client_id = settings.SM_CLIENT_ID,
+		client_secret= settings.SM_SECRET,
+		name = 'surveymonkey',
+		authorize_url = 'https://api.surveymonkey.net/oauth/authorize',
+		base_url = 'https://api.surveymonkey.net',
+		access_token_url = 'https://api.surveymonkey.net/oauth/token?api_key=%s' % settings.SM_API_KEY
+	)
+
+def build_api_url(name):
+	prekey = 'user' if 'user' in name.lower() else 'surveys'
+	# return 'https://api.surveymonkey.net/v2/%s/%s?api_key=%s' % (prekey, name, settings.SM_API_KEY)
+	return 'https://api.surveymonkey.net/v2/{}/{}?api_key={}'.format(prekey, name, settings.SM_API_KEY)
+
+def get_oauth_url():
+	surveymonkey = get_oauth_service()
+	redirect_uri = "http://127.0.0.1:8080/oa2callback"
+	params = {
+		'redirect_uri': redirect_uri,
+		'api_key': settings.SM_API_KEY,
+		'response_type': 'code'
+	}
+	url = surveymonkey.get_authorize_url(**params)
+	return url
+
+def get_session_from_response(request):
+	service = get_oauth_service()
+	return service.get_auth_session(
+		data = {
+			'code': request.GET['code'], 
+			'grant_type': 'authorization_code',
+			'redirect_uri': 'http://127.0.0.1:8080/oa2callback'
+		},
+		decoder=json.loads
+	)
+
+def get_session_from_user(user):
+	service = get_oauth_service()
+	session = OAuth2Session(client_id = settings.SM_CLIENT_ID, client_secret=settings.SM_SECRET, access_token=user.access_token, service=service)
+	return session
---
+++ hackathon/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django import http
+from django.conf import settings
+
+from .utils import *
+from .models import User
+
+def login_page(request):
+	oauth_url = get_oauth_url()
+	return render(request, 'login.html', {'oauth_url': oauth_url})
+
+def oauth2_callback(request):
+	session = get_session_from_response(request)
+	access_token = session.access_token
+
+	response_data = session.post(build_api_url('get_user_details'), params={'format': 'json'}).json()
+	username = response_data['data']['user_details']['username']
+
+	if 'SMSuccessTeam' not in response_data['data']['enterprise_details']['group_name']:
+		raise Http404
+
+	try:
+		user = User.objects.get(access_token=access_token)
+	except User.DoesNotExist:
+		user = User.objects.create(username=username, access_token=access_token)
+
+	request.session['at']=access_token
+
+	return redirect('users')
+
+def users_page(request):
+	if not 'at' in request.session and not User.objects.filter(access_token=request.session['at']).exists():
+		raise http.Http404
+
+	return render(request, 'users.html', {'users': User.objects.all()})
+
+def user_page(request, id=None):
+	if not 'at' in request.session and not User.objects.filter(access_token=request.session['at']).exists():
+		raise http.Http404
+
+	try:
+		user = User.objects.get(id=id)
+	except User.DoesNotExist:
+		raise http.Http404
+
+	session = get_session_from_user(user)
+	response_data = session.post(build_api_url('get_survey_list'), params={'format': 'json'}, data=json.dumps({
+		"order_asc": True,
+	  	"fields": [
+	    	"title",
+		    "num_responses",
+		    "date_created",
+		    "date_modified",
+		    "question_count",
+		    "language_id",
+		    "preview_url"
+		  ]	
+		}),
+		headers={'content-type': 'application/json'}
+	).json()
+
+	return render(request, 'user.html', {'surveys': response_data['data']['surveys']})
