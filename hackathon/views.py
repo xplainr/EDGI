7e8b1d61e3221378373e495c50f85d2d4f550924
Tony Mayse <tonym@surveymonkey.com>
2015-07-22 15:51:09 -0700

    forgot to remove default blank title

--- hackathon/views.py
+++ hackathon/views.py
@@ -47,7 +47,6 @@ def user_page(request, id=None, survey_title=''):
 	session = get_session_from_user(user)
 	body = {
 			"order_asc": True,
-			"title": survey_title,
 		  	"fields": [
 			    "num_responses",
 			    "date_created",
